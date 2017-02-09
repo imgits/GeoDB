1f74eba845ae88ddc1a74bcfa615d8650ede78dc
ahai <imgits@163.com>
2017-02-09 17:15:45 +0800

    a

--- GeoDB/Program.cs
+++ GeoDB/Program.cs
@@ -14,6 +14,13 @@ namespace GeoDB
         [STAThread]
         static void Main()
         {
+            //open the database
+            LookupService ls = new LookupService("/usr/local/share/GeoIP/GeoIP.dat", LookupService.GEOIP_MEMORY_CACHE);
+            //get country of the ip address
+            Country c = ls.getCountry("24.24.24.24");
+            Console.Write(" code: " + c.getCode() + "\n");
+            Console.Write(" name: " + c.getName() + "\n");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
